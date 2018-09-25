97f4c65a5123bf3c7ac8cc22151f27a27fed7692
Mwalugha Bura <mwalughabura@gmail.com>
2018-09-25 11:56:18 +0300

    Fixed to the modal form

--- JavaScript/Node/recipebook/app.js
+++ JavaScript/Node/recipebook/app.js
@@ -2,12 +2,22 @@ var express = require('express'),
     path = require('path'),
     bodyparser = require('body-parser'),
     cons = require('consolidate'),
-    dust = require('dustjs-helpers'),
     pg = require('pg'),
     app = express();
+var config = {
+        user: 'mwalugha', //env var: PGUSER
+        database: 'recipedb', //env var: PGDATABASE
+        password: 'itisjustme', //env var: PGPASSWORD
+        host: 'localhost', // Server hosting the postgres database
+        port: 5432, //env var: PGPORT
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+var pool = new pg.Pool(config);
 
 // DB connect
-var connect = "postgress://mwalugha:itisjustme@locolhost/recipedb";
+// var connectString = "postgress://mwalugha:itisjustme@locolhost:5432/recipedb";
+// var client = new pg.Client(connectString);
 
 // Assign dust engine to .dust file
 app.engine('dust', cons.dust);
@@ -24,7 +34,34 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
-    res.render('index');
+    // PG connect
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT * From public.recipes', function(err, result) {      
+            if(err) {
+                return console.error('error running query', err);
+            }
+            res.render('index', {recipes: result.rows});
+            done();
+        });
+    });
+});
+
+app.post('/add', function(req, res){
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT * From public.recipes', function(err, result) {      
+            if(err) {
+                return console.error('error running query', err);
+            }
+            res.render('index', {recipes: result.rows});
+            done();
+        });
+    });
 });
 
 // Server
